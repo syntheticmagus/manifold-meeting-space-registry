a253c4fe5f5c889676e6cae0a5a0f43b086b8d7b
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2022-11-29 11:35:55 -0800

    Initial commit with untested registry functionality.

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// TODO: This could be a database later, if needed.
+const spaceToPeerIds = new Map();
+
+app.get("/", function (request, response) {
+    response.redirect("/status");
+});
+
+app.get("/status", function (request, response) {
+    response.send(`Service live. Active spaces: ${spaceToPeerIds.size}`);
+});
+
+app.get("/debug", function (request, response) {
+    const obj = {};
+    spaceToPeerIds.forEach((peerIds, space) => {
+        obj[space] = [];
+        peerIds.forEach((peerId) => {
+            obj[space].push(peerId);
+        });
+    });
+    response.send(JSON.stringify(obj));
+});
+
+app.post("/join", function (request, response) {
+    const space = request.body.space;
+    const peerId = request.body.peerId;
+
+    if (!spaceToPeerIds.has(space)) {
+        spaceToPeerIds.set(space, new Set());
+    }
+
+    const peerIds = spaceToPeerIds.get(space);
+    peerIds.add(peerId);
+
+    response.status(200).send();
+});
+
+app.get("/members", function (request, response) {
+    const space = request.body.space;
+
+    const peerIds = spaceToPeerIds.get(space);
+    const current = [];
+    peerIds.forEach((peerId) => {
+        current.add(peerId);
+    });
+
+    response.status(200).send(JSON.stringify({ peerIds: current }));
+});
+
+app.post("/leave", function (request, response) {
+    const space = request.body.space;
+    const peerId = request.body.peerId;
+
+    if (spaceToPeerIds.has(space)) {
+        spaceToPeerIds.get(space).delete(peerId);
+    }
+
+    response.status(200).send();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`Listening on port ${port}.`) });
